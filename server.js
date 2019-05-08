86290760302c7e1c7bedb0780d7c3a877fa2129c
thiago <thiago.massuia@hotmail.com>
2019-05-07 23:16:09 -0300

    test

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ mongoose.connect(config.url, {
 
 // default route
 app.get('/', (req, res) => {
-    res.json({"message": "Welcome to ZeptoBook Product app"});
+    res.json({"message": "This is Tmassuia api"});
 });
 
 // listen on port 3000
